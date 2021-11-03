a1f5de4b2718476f90a37730f5c3aca85b9c8a94
Adel Noureddine <7352066-adelnoureddine@users.noreply.gitlab.com>
2021-11-03 11:46:02 +0100

    Update build script. Release new linux packages

--- packages/build-packages.sh
+++ packages/build-packages.sh
@@ -1,5 +1,11 @@
+# Copy binary from obj to x86_64 folder
+cp ../obj/powerjoular ./x86_64/
+
+# Generate cheksum
+sha512sum ./x86_64/powerjoular > ./x86_64/sha-512-checksum
+
 # Build RPM package
-fpm -t rpm -p powerjoular-0.1.0-x86_64.rpm
+fpm -t rpm -p powerjoular-$1-x86_64.rpm
 
 # Build DEB package
-fpm -t deb -p powerjoular-0.1.0-x86_64.deb
+fpm -t deb -p powerjoular-$1-x86_64.deb
