7dc2ba6a07aeeba4537a424ee46e857f0f2fdeb8
Adel Noureddine <adelnoureddine@users.noreply.github.com>
2023-02-07 22:18:27 +0100

    prepare 0.5 release

--- src/help_info.adb
+++ src/help_info.adb
@@ -15,7 +15,7 @@ with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
 package body Help_Info is
 
     procedure Show_Help is
-    Version_Number : constant String := "0.4";
+    Version_Number : constant String := "0.5";
     begin
         Put_Line (ESC & "[93m" & "~~ PowerJoular ~~" & ESC & "[0m");
         Put_Line ("Version " & Version_Number);
@@ -48,7 +48,7 @@ package body Help_Info is
         Put_Line ("PowerJoular is written and maintained by Dr Adel Noureddine from the University of Pau and the Pays de l'Adour");
         Put_Line ("--------------------------");
         Put_Line (ESC & "[93m" & "Copyright:" & ESC & "[0m");
-        Put_Line ("Copyright (c) 2020-2021, Adel Noureddine. PowerJoular is licensed under the GNU GPL 3 license only (GPL-3.0-only)");
+        Put_Line ("Copyright (c) 2020-2023, Adel Noureddine. PowerJoular is licensed under the GNU GPL 3 license only (GPL-3.0-only)");
     end;
 
 end Help_Info;
