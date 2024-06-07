af3b0f580cac1fe7c6d498c51d7c73e85b76876b
Axel TERRIER <47625927+axelterrier@users.noreply.github.com>
2024-06-07 15:42:13 +0200

    Create .rpm packages for 3 architectures

--- installer/create-packages-rpm.sh
+++ installer/create-packages-rpm.sh
@@ -1,24 +1,44 @@
-#!/bin/bash
+#!/bin/sh
 
-# Verification de la presence de rpmbuild
-if ! command -v rpmbuild &> /dev/null; then
-    echo "rpmbuild could not be found. This script is intended to run on a system with RPM package support."
-    exit 1
-fi
+# Define architectures
+ARCHS=("x86_64" "aarch64" "armhfp")
 
-# Creation des repertoires sources pour rpmbuild
-mkdir -p ~/rpmbuild/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-# Copie des fichiers sources
-cp ../obj/powerjoular ~/rpmbuild/SOURCES/
-cp powerjoular.service ~/rpmbuild/SOURCES/
+# Loop over each architecture
+for ARCH in "${ARCHS[@]}"
+do
+    rm -rf $SCRIPT_DIR/$ARCH
+    
+    # Create folder for architecture
+    mkdir -p $SCRIPT_DIR/$ARCH
 
-# Copie et préparation du fichier spec
-cp powerjoular.spec ~/rpmbuild/SPECS/
+    # Copy binary and service files to their respective directories
+    cp $SCRIPT_DIR/../obj/powerjoular $SCRIPT_DIR/$ARCH/
+    cp $SCRIPT_DIR/../systemd/powerjoular.service $SCRIPT_DIR/$ARCH/
 
-# Construction du RPM
-rpmbuild -ba ~/rpmbuild/SPECS/powerjoular.spec
+    # Prepare the RPM build environment
+    mkdir -p $SCRIPT_DIR/$ARCH/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+    cp $SCRIPT_DIR/powerjoular.spec $SCRIPT_DIR/$ARCH/rpmbuild/SPECS/
 
-# Verification et nettoyage
-echo "Package RPM construit :"
-find ~/rpmbuild/RPMS/ -type f -name "*.rpm"
+    # Copy the sources accordingly
+    cp $SCRIPT_DIR/$ARCH/powerjoular $SCRIPT_DIR/$ARCH/rpmbuild/SOURCES/
+    cp $SCRIPT_DIR/$ARCH/powerjoular.service $SCRIPT_DIR/$ARCH/rpmbuild/SOURCES/
+
+    # Build the RPM package
+    cd $SCRIPT_DIR/$ARCH
+    rpmbuild -ba rpmbuild/SPECS/powerjoular.spec --define "_topdir $(pwd)/rpmbuild" --target $ARCH
+
+    # Move the created RPM to a more accessible location
+    find rpmbuild/RPMS/ -name '*.rpm' -exec mv {} $SCRIPT_DIR/$ARCH/ \;
+done
+
+# Combine all RPM packages into a single directory for convenience
+mkdir -p $SCRIPT_DIR/all_rpms
+for ARCH in "${ARCHS[@]}"
+do
+    mv $SCRIPT_DIR/$ARCH/*.rpm $SCRIPT_DIR/all_rpms/
+done
+
+echo "All RPM packages have been created and moved to the 'all_rpms' directory in $SCRIPT_DIR."
