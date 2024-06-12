001c1d1448bc35577fb6696e4dc66d0051ea15c2
Axel TERRIER <47625927+axelterrier@users.noreply.github.com>
2024-06-12 15:42:34 +0200

    Cross compilation available through the script
    The script now create a .pkg.tar.zst for each architecture from the PKGBUILD
    
    The packages are stored in the "arch_source_packages"

--- installer/create-packages-arch-source.sh
+++ installer/create-packages-arch-source.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Function to check user permissions to write to current directory
+# Function to check user permissions to write to the current directory
 if [ ! -w "$(pwd)" ]; then
     echo "ERROR: You don't have write permission on the directory $(pwd)."
     exit 1
 fi
+
+# List of architectures to cross-compile for
+ARCHS=("x86_64" "aarch64" "armv7h")
 
 # Source directory for PKGBUILD
 PKG_DIR="arch_pkgbuild"
@@ -15,17 +18,50 @@ mkdir -p $PKG_DIR $OUTPUT_DIR
 # Copy the PKGBUILD file to the build directory
 cp PKGBUILD $PKG_DIR/
 
-# Changer de répertoire pour PKGBUILD
+# Change directory to PKGBUILD
 cd $PKG_DIR
 
-# Change directory to PKGBUILD
-makepkg
+# Loop through each architecture and cross-compile
+for ARCH in "${ARCHS[@]}"; do
+    # Set up the environment for cross-compiling
+    case $ARCH in
+        "x86_64")
+            CROSS_COMPILE=""
+            ;;
+        "aarch64")
+            CROSS_COMPILE="aarch64-linux-gnu-"
+            ;;
+        "armv7h")
+            CROSS_COMPILE="arm-linux-gnueabihf-"
+            ;;
+        *)
+            echo "Unsupported architecture: $ARCH"
+            exit 1
+            ;;
+    esac
 
-#  Move the generated package to the output directory
-mv *.pkg.tar.zst ../$OUTPUT_DIR/
+    # Export the necessary environment variables
+    export CC="${CROSS_COMPILE}gcc"
+    export CXX="${CROSS_COMPILE}g++"
+    export AR="${CROSS_COMPILE}ar"
+    export RANLIB="${CROSS_COMPILE}ranlib"
 
-#  Clean up temporary directories
+    # Update PKGBUILD architecture
+    sed -i "s/^arch=.*/arch=('$ARCH')/" PKGBUILD
+
+     # Build the package
+    makepkg -Acfs --noconfirm
+
+    # Move the generated package to the output directory with architecture name
+    mv *.pkg.tar.zst ../$OUTPUT_DIR/powerjoular-$ARCH.pkg.tar.zst
+
+    # Clean up the build directory for the next architecture
+    rm -rf src pkg
+done
+
+# Clean up temporary directories
 cd ..
 rm -rf $PKG_DIR
 
-echo "Arch package has been created and moved to the '$OUTPUT_DIR' directory."
+echo "Arch packages have been created and moved to the '$OUTPUT_DIR' directory."
+
