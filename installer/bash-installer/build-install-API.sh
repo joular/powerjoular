088e1816835e7f0d144010177ae63ad1ea8dbe0e
fred <support@qo-op.com>
2024-09-05 18:23:27 +0200

    PowerJoular 22407 API service

--- installer/bash-installer/build-install-API.sh
+++ installer/bash-installer/build-install-API.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+ME="${0##*/}"
 
 # Go back to main directory
-cd ../..
+cd $MY_PATH/../..
 
 # Create obj/ folder it not exist
 mkdir -p obj
@@ -16,10 +19,16 @@ sudo cp ./systemd/powerjoular_api.py /usr/local/bin
 sudo chmod +x /usr/local/bin/powerjoular_api.py
 
 # Install systemd service
-cat ./systemd/powerjoular22407.service | sed "s~_USER_~${USER}~g" > /tmp/powerjoular.service
-sudo cp /tmp/powerjoular.service /etc/systemd/system/powerjoular.service
+sudo cp -f ./systemd/powerjoular.service /etc/systemd/system/
+
+cat ./systemd/powerjoular22407.service | sed "s~_USER_~${USER}~g" > /tmp/powerjoularAPI.service
+sudo cp /tmp/powerjoularAPI.service /etc/systemd/system/
 
 
 echo "Run
+sudo systemctl daemon-reload
+
 sudo systemctl enable powerjoular
-sudo systemctl start powerjoular"
+sudo systemctl enable powerjoularAPI
+sudo systemctl start powerjoular
+sudo systemctl start powerjoularAPI"
